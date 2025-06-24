9a99ec03b7c518d3f333d4c0771f5fecd1c3df69
mazbaul20 <mazbaul20@gmail.com>
2025-06-24 17:48:54 +0600

    18.Product Update and Delete with Image Handling - 04

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -52,5 +53,68 @@ class ProductController extends Controller
     public function ProductById(Request $request){
         $user_id = $request->header('id');
         return Product::where('id',$request->input('id'))->where('user_id',$user_id)->first();
+    }//End Method
+
+    public function ProductUpdate(Request $request){
+        $user_id = $request->header('id');
+
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'unit' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        $product = Product::where('user_id',$user_id)->findOrFail($request->input('id'));
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->unit = $request->input('unit');
+        $product->category_id = $request->input('category_id');
+
+        if($request->hasFile('image')){
+            if($product->image && file_exists(public_path($product->image))){
+                unlink(public_path($product->image));
+            }
+            $request->validate([
+                'image' => 'nullable|image|mimes:jpeg,png,gif,svg,webp|max:2048',
+            ]);
+
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $filePath = 'uploads/'.$filename;
+            $file->move(public_path('uploads'), $filename);
+            $product->image = $filePath;
+        }
+
+        $product->save();
+
+        return response()->json([
+            'status' => "success",
+            'message' => "Product updated successfully",
+        ]);
+    }//End Method
+
+    public function ProductDelete(Request $request,$id){
+        try{
+            $user_id = $request->header('id');
+
+            $product = Product::where('user_id',$user_id)->findOrFail($id);
+
+            if($product->image && file_exists(public_path($product->image))){
+                unlink(public_path($product->image));
+            }
+
+            $product->delete();
+            
+            return response()->json([
+                'status' => "success",
+                'message' => "Product deleted successfully",
+            ]);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => "failed",
+                'message' => 'Something went wrong, please try again later',
+            ]);
+        }
     }//End Method
 }
